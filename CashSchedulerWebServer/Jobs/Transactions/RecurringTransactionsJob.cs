121dce3ae8286aac953569e109526452f401fe99
Ilya Matsuev <ilya.matsuev.personal@gmail.com>
2021-04-27 18:37:28 +0300

    Add notify on transaction jobs failure

--- CashSchedulerWebServer/Jobs/Transactions/RecurringTransactionsJob.cs
+++ CashSchedulerWebServer/Jobs/Transactions/RecurringTransactionsJob.cs
@@ -7,84 +7,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CashSchedulerWebServer.Services.Contracts;
+using CashSchedulerWebServer.WebServices.Contracts;
+using CashSchedulerWebServer.WebServices.Salesforce.SObjects;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CashSchedulerWebServer.Jobs.Transactions
 {
     public class RecurringTransactionsJob : IJob
     {
         private CashSchedulerContext CashSchedulerContext { get; }
+        private IUserNotificationService NotificationService { get; }
+        private ISalesforceApiWebService SalesforceService { get; }
 
-        public RecurringTransactionsJob(CashSchedulerContext cashSchedulerContext)
+        public RecurringTransactionsJob(CashSchedulerContext cashSchedulerContext, IServiceProvider serviceProvider)
         {
             CashSchedulerContext = cashSchedulerContext;
+            NotificationService = serviceProvider.GetService<IUserNotificationService>();
+            SalesforceService = serviceProvider.GetService<ISalesforceApiWebService>();
         }
 
 
         public Task Execute(IJobExecutionContext context)
         {
-            var now = DateTime.UtcNow;
             Console.WriteLine($"Running the {context.JobDetail.Description}");
-            var recurringTransactions = CashSchedulerContext.RegularTransactions
-                .Where(t => t.NextTransactionDate.Date == now.Date)
-                .Include(t => t.Category)
-                .Include(t => t.Category.Type)
-                .Include(t => t.User)
-                .Include(t => t.Wallet)
-                .ToList();
-
-            var singleTransactionsToBeCreated = new List<Transaction>();
-            var recurringTransactionsToBeUpdated = new List<RegularTransaction>();
-            var walletsToUpdateBalance = recurringTransactions.GroupBy(t => t.Wallet).Select(t =>
-            {
-                var wallet = t.Key;
-                double initBalance = wallet.Balance;
-
-                wallet.Balance += t.Sum(
-                    rt => rt.Category.Type.Name == TransactionType.Options.Income.ToString()
-                        ? rt.Amount
-                        : -rt.Amount
-                );
-                if (wallet.Balance < 0)
-                {
-                    wallet.Balance = initBalance;
-                    // TODO: send email notifying that we cannot create transaction for future since the balance will become less than 0
-                }
-                else
-                {
-                    singleTransactionsToBeCreated.AddRange(t.Select(rt => new Transaction
-                    {
-                        Title = rt.Title,
-                        User = rt.User,
-                        Wallet = rt.Wallet,
-                        Category = rt.Category,
-                        Amount = rt.Amount
-                    }));
-
-                    recurringTransactionsToBeUpdated.AddRange(t.Select(rt =>
-                    {
-                        rt.Date = DateTime.Today;
-                        rt.NextTransactionDate = GetNextDateByInterval(rt);
-                        return rt;
-                    }));
-                }
-
-                return wallet;
-            }).ToList();
 
             using var dmlTransaction = CashSchedulerContext.Database.BeginTransaction();
             try
             {
-                CashSchedulerContext.Transactions.AddRange(singleTransactionsToBeCreated);
+                var newTransactions = new List<Transaction>();
+                var updatedRecurringTransactions = new List<RegularTransaction>();
+                var updatedWallets = GetUpdatedWallets(
+                    GetTodayTransactions(),
+                    newTransactions,
+                    updatedRecurringTransactions
+                );
+
+                CashSchedulerContext.Transactions.AddRange(newTransactions);
                 CashSchedulerContext.SaveChanges();
-                CashSchedulerContext.RegularTransactions.UpdateRange(recurringTransactionsToBeUpdated);
+
+                CashSchedulerContext.RegularTransactions.UpdateRange(updatedRecurringTransactions);
                 CashSchedulerContext.SaveChanges();
-                CashSchedulerContext.Wallets.UpdateRange(walletsToUpdateBalance);
+
+                CashSchedulerContext.Wallets.UpdateRange(updatedWallets);
                 CashSchedulerContext.SaveChanges();
 
                 dmlTransaction.Commit();
+                
+                SalesforceService.UpsertSObjects(newTransactions
+                        .Select(t => new SfTransaction(t, t.Id)).ToList<SfObject>())
+                    .GetAwaiter().GetResult();
+                
+                SalesforceService.UpsertSObjects(updatedRecurringTransactions
+                        .Select(t => new SfRecurringTransaction(t, t.Id)).ToList<SfObject>())
+                    .GetAwaiter().GetResult();
+                
+                SalesforceService.UpsertSObjects(updatedWallets.Select(w => new SfWallet(w, w.Id)).ToList<SfObject>())
+                    .GetAwaiter().GetResult();
 
-                Console.WriteLine($"{singleTransactionsToBeCreated.Count} single transactions were created");
-                Console.WriteLine($"{walletsToUpdateBalance.Count} wallets were updated");
+                Console.WriteLine($"{newTransactions.Count} single transactions were created");
+                Console.WriteLine($"{updatedRecurringTransactions.Count} recurring transactions were updated");
+                Console.WriteLine($"{updatedWallets.Count} wallets were updated");
             }
             catch (Exception error)
             {
@@ -95,22 +78,107 @@ namespace CashSchedulerWebServer.Jobs.Transactions
             return Task.CompletedTask;
         }
 
+
+        private List<RegularTransaction> GetTodayTransactions()
+        {
+            var now = DateTime.UtcNow;
+            return CashSchedulerContext.RegularTransactions
+                .Where(t => t.NextTransactionDate.Date == now.Date)
+                .Include(t => t.Category)
+                .Include(t => t.Category.Type)
+                .Include(t => t.User)
+                .Include(t => t.Wallet)
+                .ToList();
+        }
+        
+        private List<Wallet> GetUpdatedWallets(
+            List<RegularTransaction> transactions,
+            List<Transaction> newTransactions,
+            List<RegularTransaction> updatedRecurringTransactions)
+        {
+            return transactions.GroupBy(t => t.Wallet)
+                .Select(t => UpdateWallet(t, newTransactions, updatedRecurringTransactions))
+                .ToList();
+        }
+
+        private Wallet UpdateWallet(
+            IGrouping<Wallet, RegularTransaction> transactionsByWallet,
+            List<Transaction> newTransactions,
+            List<RegularTransaction> updatedRecurringTransactions)
+        {
+            var wallet = transactionsByWallet.Key;
+            double initBalance = wallet.Balance;
+
+            wallet.Balance += transactionsByWallet.Sum(CalculateAmount);
+
+            if (wallet.Balance < 0)
+            {
+                wallet.Balance = initBalance;
+                NotifyNotEnoughMoney(transactionsByWallet);
+            }
+            else
+            {
+                newTransactions.AddRange(transactionsByWallet.Select(rt => new Transaction
+                {
+                    Title = rt.Title,
+                    User = rt.User,
+                    Wallet = rt.Wallet,
+                    Category = rt.Category,
+                    Amount = rt.Amount
+                }));
+
+                updatedRecurringTransactions.AddRange(transactionsByWallet.Select(rt =>
+                {
+                    rt.Date = DateTime.Today;
+                    rt.NextTransactionDate = GetNextDateByInterval(rt);
+                    return rt;
+                }));
+            }
+            return wallet;
+        }
+
+        private string GetTransactionName(RegularTransaction transaction)
+        {
+            return string.IsNullOrEmpty(transaction.Title)
+                ? transaction.Category.Name
+                : transaction.Title;
+        }
+
+        private double CalculateAmount(RegularTransaction transaction)
+        {
+            return transaction.Category.Type.Name == TransactionType.Options.Income.ToString()
+                ? transaction.Amount
+                : -transaction.Amount;
+        }
+
         private DateTime GetNextDateByInterval(RegularTransaction transaction)
         {
             var intervals = new Dictionary<string, Func<DateTime, DateTime>>
             {
-                {RegularTransaction.IntervalOptions.Day.ToString().ToLower(), (date) => date.AddDays(1) },
-                {RegularTransaction.IntervalOptions.Week.ToString().ToLower(), (date) => date.AddDays(7) },
-                {RegularTransaction.IntervalOptions.Month.ToString().ToLower(), (date) => date.AddMonths(1) },
-                {RegularTransaction.IntervalOptions.Year.ToString().ToLower(), (date) => date.AddYears(1) }
+                {RegularTransaction.IntervalOptions.Day.ToString().ToLower(), (date) => date.AddDays(1)},
+                {RegularTransaction.IntervalOptions.Week.ToString().ToLower(), (date) => date.AddDays(7)},
+                {RegularTransaction.IntervalOptions.Month.ToString().ToLower(), (date) => date.AddMonths(1)},
+                {RegularTransaction.IntervalOptions.Year.ToString().ToLower(), (date) => date.AddYears(1)}
             };
 
             if (!intervals.ContainsKey(transaction.Interval))
             {
-                throw new CashSchedulerException($"There is no such value for interval: {transaction.Interval}");                
+                throw new CashSchedulerException($"There is no such value for interval: {transaction.Interval}");
             }
 
             return intervals[transaction.Interval](transaction.NextTransactionDate);
+        }
+
+        private void NotifyNotEnoughMoney(IGrouping<Wallet, RegularTransaction> transactionsByWallet)
+        {
+            var transactionNames = string.Join(',', transactionsByWallet.Select(GetTransactionName));
+
+            NotificationService.Create(new UserNotification
+            {
+                Title = "Future transaction charge",
+                Content = $"We're sorry but you don't have enough money on the wallet \"{transactionsByWallet.Key.Name}\" for the transactions: {transactionNames}",
+                User = transactionsByWallet.Key.User
+            });
         }
     }
 }
--- CashSchedulerWebServer/Jobs/Transactions/TransactionsJob.cs
+++ CashSchedulerWebServer/Jobs/Transactions/TransactionsJob.cs
@@ -3,59 +3,46 @@ using CashSchedulerWebServer.Models;
 using Microsoft.EntityFrameworkCore;
 using Quartz;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CashSchedulerWebServer.Services.Contracts;
+using CashSchedulerWebServer.WebServices.Contracts;
+using CashSchedulerWebServer.WebServices.Salesforce.SObjects;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CashSchedulerWebServer.Jobs.Transactions
 {
     public class TransactionsJob : IJob
     {
         private CashSchedulerContext CashSchedulerContext { get; }
+        private IUserNotificationService NotificationService { get; }
+        private ISalesforceApiWebService SalesforceService { get; }
 
-        public TransactionsJob(CashSchedulerContext cashSchedulerContext)
+        public TransactionsJob(CashSchedulerContext cashSchedulerContext, IServiceProvider serviceProvider)
         {
             CashSchedulerContext = cashSchedulerContext;
+            NotificationService = serviceProvider.GetService<IUserNotificationService>();
+            SalesforceService = serviceProvider.GetService<ISalesforceApiWebService>();
         }
 
 
         public Task Execute(IJobExecutionContext context)
         {
-            var now = DateTime.UtcNow;
             Console.WriteLine($"Running the {context.JobDetail.Description}");
-            var transactions = CashSchedulerContext.Transactions
-                .Where(t => t.Date.Date == now.Date)
-                .Include(t => t.Category)
-                .Include(t => t.Category.Type)
-                .Include(t => t.Wallet)
-                .ToList();
-
-            var walletsToUpdateBalance = transactions.GroupBy(t => t.Wallet).Select(t =>
-            {
-                var wallet = t.Key;
-                double initBalance = wallet.Balance;
-                wallet.Balance += t.Sum(
-                    transaction => transaction.Category.Type.Name == TransactionType.Options.Income.ToString() 
-                        ? transaction.Amount 
-                        : -transaction.Amount
-                );
-                if (wallet.Balance < 0)
-                {
-                    wallet.Balance = initBalance;
-                    // TODO: send email notifying that we cannot create transaction for future since the balance will become less than 0
-                }
-                return wallet;
-            }).ToList();
-
 
             using var dmlTransaction = CashSchedulerContext.Database.BeginTransaction();
             try
             {
-                CashSchedulerContext.Wallets.UpdateRange(walletsToUpdateBalance);
+                var updatedWallets = GetUpdatedWallets(GetTodayTransactions());
+
+                CashSchedulerContext.Wallets.UpdateRange(updatedWallets);
                 CashSchedulerContext.SaveChanges();
 
                 dmlTransaction.Commit();
+                SalesforceService.UpsertSObjects(updatedWallets.Select(w => new SfWallet(w, w.Id)).ToList<SfObject>());
 
-                Console.WriteLine($"{walletsToUpdateBalance.Count} wallets were updated");
+                Console.WriteLine($"{updatedWallets.Count} wallets were updated");
             }
             catch (Exception error)
             {
@@ -64,6 +51,63 @@ namespace CashSchedulerWebServer.Jobs.Transactions
             }
 
             return Task.CompletedTask;
+        }
+
+
+        private List<Transaction> GetTodayTransactions()
+        {
+            var now = DateTime.UtcNow;
+            return CashSchedulerContext.Transactions
+                .Where(t => t.Date.Date == now.Date)
+                .Include(t => t.Category)
+                .Include(t => t.Category.Type)
+                .Include(t => t.Wallet)
+                .ToList();
+        }
+
+        private List<Wallet> GetUpdatedWallets(List<Transaction> transactions)
+        {
+            return transactions.GroupBy(t => t.Wallet).Select(UpdateWallet).ToList();
+        }
+
+        private Wallet UpdateWallet(IGrouping<Wallet, Transaction> transactionsByWallet)
+        {
+            var wallet = transactionsByWallet.Key;
+            double initBalance = wallet.Balance;
+
+            wallet.Balance += transactionsByWallet.Sum(CalculateAmount);
+            if (wallet.Balance < 0)
+            {
+                NotifyNotEnoughMoney(transactionsByWallet);
+                wallet.Balance = initBalance;
+            }
+            return wallet;
+        }
+
+        private string GetTransactionName(Transaction transaction)
+        {
+            return string.IsNullOrEmpty(transaction.Title)
+                ? transaction.Category.Name
+                : transaction.Title;
+        }
+
+        private double CalculateAmount(Transaction transaction)
+        {
+            return transaction.Category.Type.Name == TransactionType.Options.Income.ToString()
+                ? transaction.Amount
+                : -transaction.Amount;
+        }
+
+        private void NotifyNotEnoughMoney(IGrouping<Wallet, Transaction> transactionsByWallet)
+        {
+            var transactionNames = string.Join(',', transactionsByWallet.Select(GetTransactionName));
+
+            NotificationService.Create(new UserNotification
+            {
+                Title = "Future transaction charge",
+                Content = $"We're sorry but you don't have enough money on the wallet \"{transactionsByWallet.Key.Name}\" for the transactions: {transactionNames}",
+                User = transactionsByWallet.Key.User
+            });
         }
     }
 }
